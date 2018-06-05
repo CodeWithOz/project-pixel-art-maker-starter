b6bc9c69640bd209f1211c7dadff319e85c28f24
Uche Ozoemena <ucheozoemena@gmail.com>
2018-06-05 18:09:36 +0100

    Outline basic steps of makeLine() function

--- designs.js
+++ designs.js
@@ -4,7 +4,11 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+  // save the selected grid dimensions
+  // save the selected color
+  // create the table based on the grid values
+    // essentially, I need to define what each row will look like
+      // the row will have the selected number of columns
+    // then replicate it as many times as the selected number of rows
+    // then use event delegation to add necessary event listeners on the parent table element
 }
