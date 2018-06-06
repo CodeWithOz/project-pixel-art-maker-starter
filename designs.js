994d013a7c7a3a1499d5089f10151d027b97aac8
Uche Ozoemena <ucheozoemena@gmail.com>
2018-06-06 14:31:16 +0100

    Wrap all code inside $(document).ready()

--- designs.js
+++ designs.js
@@ -1,23 +1,25 @@
-var cellColor;
+$(document).ready(function() {
+  var cellColor;
 
-cellColor = $('#colorPicker').val();
-$('#colorPicker').change(function() {
-  cellColor = $( this ).val();
+  cellColor = $('#colorPicker').val();
+  $('#colorPicker').change(function() {
+    cellColor = $( this ).val();
+  });
+
+  $('#sizePicker').submit(makeGrid);
+
+  function makeGrid(event) {
+    var rows, columns;
+
+    rows = $('#inputHeight').val();
+    columns = $('#inputWidth').val();
+
+    // create the table based on the grid values
+      // no need for a `thead`, go straight to `tbody`
+      // <tbody> needs to come before the rows
+      // essentially, I need to define what each row will look like
+        // the row will have the selected number of columns
+      // then replicate it as many times as the selected number of rows
+      // then use event delegation to add necessary event listeners on the parent table element
+  }
 });
-
-$('#sizePicker').submit(makeGrid);
-
-function makeGrid(event) {
-  var rows, columns;
-
-  rows = $('#inputHeight').val();
-  columns = $('#inputWidth').val();
-
-  // create the table based on the grid values
-    // no need for a `thead`, go straight to `tbody`
-    // <tbody> needs to come before the rows
-    // essentially, I need to define what each row will look like
-      // the row will have the selected number of columns
-    // then replicate it as many times as the selected number of rows
-    // then use event delegation to add necessary event listeners on the parent table element
-}
